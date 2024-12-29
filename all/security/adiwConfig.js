8defe938a8217b874bb77523d77ef19743c4b2f8
Xiaoyy404 <zamrpedia@gmail.com>
2024-12-29 10:09:16 +0700

    Update adiwConfig.js

--- all/security/adiwConfig.js
+++ all/security/adiwConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   'approval': {
-    'num': '6289508082845',
+    'num': '6285736458548',
     'text': "oke",
     'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
     'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
   },
-  'creatorScript': '6289508082845',
+  'creatorScript': '6285736458548',
   'filePath': "./all/approval",
   'checkFilePath': "../setting/elaina.js",
   'codeToDetect': 'main();'
